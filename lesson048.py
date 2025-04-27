c2af00e855ecd08f3eb3fedc6e2a3fc77cf4700a
Mark Peters <mapeters101@earthlink.net>
2025-04-27 00:26:50 -0400

    Work on lesson048.

---
+++ lesson048.py
@@ -0,0 +1,41 @@
+
+class Node:
+    def __init__(self, data, parent):
+        self.data = data
+        self.left_node = None
+        self.right_node = None
+        # Crucial when implementing the remove function
+        self.parent = parent
+
+class BinarySearchTree:
+    def __init__(self):
+        # We can access the root node exclusively
+        self.root = None
+
+    def insert(self, data):
+        # First node in the BST
+        if self.root is None:
+            self.root = Node(data)
+        # BST is not empty
+        else:
+            self.insert_node(data, self.root)
+
+    def insert_node(self, data, node):
+        # Go to the left subtree
+        if data < node.data:
+            if node.left_node is not None:
+                # Left node exists, so we keep going
+                self.insert_node(data, node.left_node)
+            else:
+                # There is no left child, so we create one
+                node.left_node = Node(data, node)
+        else:
+            if node.right_node is not None:
+                # Right node exists, so we keep going
+                self.insert_node(data, node.right_node)
+            else:
+                # There is no right child, so we create one
+                node.right_node = Node(data, node)
+
+
+
